6f0886bdc56e2031b9d97875cfda48057bb08464
samer <samirmamer@gmail.com>
2021-11-12 23:24:33 -0500

    Add gaps - https://dwm.suckless.org/patches/fullgaps/dwm-fullgaps-6.2.diff

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -9,7 +10,7 @@ static const char *fonts[]          = { "SauceCodePro Nerd Font:size=11" };
 static const char dmenufont[]       = "SauceCodePro Nerd Font:size=11";
 static const char col_norm_fg[]	    = "#fbf1c7";
 static const char col_norm_bg[]	    = "#282828";
-static const char col_norm_border[] = "#282828";
+static const char col_norm_border[] = "#fbf1c7";
 
 static const char col_sel_fg[]	    = "#fbf1c7";
 static const char col_sel_bg[]	    = "#3c3836";
@@ -125,6 +126,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
